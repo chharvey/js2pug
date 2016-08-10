217208a287af4ee4b2bf0e94d3885fc966c7dd26
Chris Harvey <chrisharvey2pi@gmail.com>
2016-08-10 16:01:09 -0400

    update formatting & readme

--- README.md
+++ README.md
@@ -9,39 +9,53 @@ containing unbuffered block code.
 This is useful if you want to write your (server-side) Pug data in actual Javascript.
 
 ## Installation & Usage
-```
-$ npm install [-g] js2pug
-```
 
-Currently only available on the command line. Will introduce programmatic support later.
-
+### Command Line
+To run from the command line, install globally.
 ```
+$ npm install -g js2pug
 $ js2pug [--jade] file.js
 ```
-where `file.js` is the name of your source file, and
+where `file.js` is the name of your source file (relative path), and
 `file.js.pug` will be the name of the output file (in the same directory).
 
 The `--jade` option specifies `.jade` instead of `.pug` for the output file extension.
 
+### Programmatic
+To use in your own node module, install as a dependency.
+```
+$ npm install --save[-dev] js2pug
+```
+```js
+var js2pug = require('js2pug')
+
+js2pug('file.js')       // file.js -> file.js.pug
+js2pug('file.js', true) // file.js -> file.js.jade
+```
+The function `js2pug()` returns `undefined`. (This may change at a later time.)
+
 ## Example
 
-src (`file.js`):
+src: `file.js`:
 ```js
 var MyClass = (function () {
+  // CONSTRUCTOR
   function MyClass() {
-    // CONSTRUCTOR
   }
   return MyClass
 })()
 ```
 
-out (`file.js.pug` or `file.js.jade`):
+out: `file.js.pug` or `file.js.jade`:
 ```jade
 -
   var MyClass = (function () {
+    //- CONSTRUCTOR
     function MyClass() {
-      //- CONSTRUCTOR
     }
     return MyClass
   })()
 ```
+
+## Changelog
+On [GitHub](https://github.com/chharvey/js2pug/releases).
--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-// Algorithm
-// - take contents of file, named 'file.js'
-// - search-and-replace every `\n` (U+000A line feed) with
-//   `\n  ` (U+000A line feed, U+0020 space, U+0020 space) (for indentation)
-// - prepend `-\n` (U+002D hyphen-minus, U+000A line feed)
-// - search-and-replace every `//` with `//-` (for unbuffered comments)
-// - write to new file 'file.js.pug'
-
 var
     fs = require('fs')
   , path = require('path')
   , program = require('commander')
 
+/**
+ * Converts a js file into a Pug file containing unbuffered block code.
+ * @param  {string} file    the file (relative directory) to convert
+ * @param  {boolean} jade   whether to use file extension `.jade` (truthy) or `.pug` (falsy)
+ * @return {undefined}      returns undefined
+ */
 module.exports = function js2pug(file, jade) {
-var sourcefile = path.parse(path.normalize(process.cwd() + '/' + file))
-  , sourcefilestr = sourcefile.dir + '/' + sourcefile.base
+  // Algorithm
+  // - take contents of file, named 'file.js'
+  // - search-and-replace every `\n` (U+000A line feed) with
+  //   `\n  ` (U+000A line feed, U+0020 space, U+0020 space) (for indentation)
+  // - prepend `-\n` (U+002D hyphen-minus, U+000A line feed)
+  // - search-and-replace every `//` with `//-` (for unbuffered comments)
+  // - write to new file 'file.js.pug'
+  var sourcefile = path.parse(path.normalize(process.cwd() + '/' + file))
+    , sourcefilestr = sourcefile.dir + '/' + sourcefile.base
 
-fs.readFile(sourcefilestr, 'utf8', function (err, data) {
-  if (err) return console.error('There was an error reading the file: ', err)
-  var outfilestr = sourcefilestr + ((jade) ? '.jade' : '.pug')
-    , outfile = path.parse(outfilestr)
-  data = '-\n  ' + data.replace(/\n/g,'\n  ')
-  data = data.replace(/\/\//g, '//-')
-  fs.writeFile(outfilestr, data, function (err) {
-    if (err) return console.error('There was an error writing the file: ', err)
-    console.log('Success! ' + sourcefile.base + ' > ' + outfile.base)
+  fs.readFile(sourcefilestr, 'utf8', function (err, data) {
+    if (err) return console.error('There was an error reading the file: ', err)
+    var outfilestr = sourcefilestr + ((jade) ? '.jade' : '.pug')
+      , outfile = path.parse(outfilestr)
+    data = '-\n  ' + data.replace(/\n/g,'\n  ')
+    data = data.replace(/\/\//g, '//-')
+    fs.writeFile(outfilestr, data, function (err) {
+      if (err) return console.error('There was an error writing the file: ', err)
+      console.log('Success! ' + sourcefile.base + ' > ' + outfile.base)
+    })
   })
-})
-return
+  return
 }
